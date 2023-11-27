0fbba159a004a0f74ea1f8c3e178e4baafeba8a5
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2023-11-27 17:16:28 +0000

    Obj creation + waves animation

--- sketch.js
+++ sketch.js
@@ -6,8 +6,10 @@ let grassColor
 let lake
 let flowers = []
 let angle = 0
-let radius1 = 85;
-let radius2 = 50;
+// let radius1 = 85;
+// let radius2 = 50;
+let radius = [85,50]
+let radiusWaves = [lakeX, lakeY]
 let speed = 0.01;
 // the center of our rotation:
 let centerX = 0;
@@ -15,15 +17,16 @@ let centerY = 0;
 
 let grassX=[]
 let grassY=[]
-let position1=[]
-let position2=[]
-let position3=[]
-let position4=[]
-let position5=[]
-let position6=[]
-let position7=[]
+// let position1=[]
+// let position2=[]
+// let position3=[]
+// let position4=[]
+// let position5=[]
+// let position6=[]
+// let position7=[]
+let positions = {position1:[], position2:[], position3:[], position4:[], position5:[], position6:[], position7:[]}
 
-let angles = []
+// let angles = []
 
 function setup() {
   const w = window.innerWidth
@@ -40,14 +43,21 @@ function setup() {
   for(let i=0; i<50; i++){
     grassX[i]=random(0,w)
     grassY[i]=random(0,h)
-    position1[i]=random(5,10)
-    position2[i]=random(8,14)
-    position3[i]=random(10,15)
-    position4[i]=random(6,8)
-    position5[i]=random(15,20)
-    position6[i]=random(8,14)
-    position7[i]=random(20,25)
-    angles[i]=random(-1,1)
+    // position1[i]=random(5,10)
+    // position2[i]=random(8,14)
+    // position3[i]=random(10,15)
+    // position4[i]=random(6,8)
+    // position5[i]=random(15,20)
+    // position6[i]=random(8,14)
+    // position7[i]=random(20,25)
+    positions.position1[i]=random(5,10)
+    positions.position2[i]=random(8,14)
+    positions.position3[i]=random(10,15)
+    positions.position4[i]=random(6,8)
+    positions.position5[i]=random(15,20)
+    positions.position6[i]=random(8,14)
+    positions.position7[i]=random(20,25)
+    // angles[i]=random(-1,1)
   }
 
   console.log(w/400);
@@ -94,10 +104,10 @@ function draw(){
     
     beginShape();
 	  vertex(grassX[j],grassY[j]);
-	  vertex(grassX[j]+position1[j],grassY[j]-position2[j]);
-	  vertex(grassX[j]+position3[j],grassY[j]-position4[j]);
-	  vertex(grassX[j]+position5[j],grassY[j]-position6[j]);
-    vertex(grassX[j]+position7[j],grassY[j]);
+	  vertex(grassX[j]+positions.position1[j],grassY[j]-positions.position2[j]);
+	  vertex(grassX[j]+positions.position3[j],grassY[j]-positions.position4[j]);
+	  vertex(grassX[j]+positions.position5[j],grassY[j]-positions.position6[j]);
+    vertex(grassX[j]+positions.position7[j],grassY[j]);
 	  endShape();
     
     
@@ -125,7 +135,6 @@ function draw(){
     addFish()
   }
  
-
 }
 
 function mousePressed(){
@@ -139,7 +148,6 @@ function mousePressed(){
     let nPetals = Math.floor(random(9,14))
 
     flowers.push({x:mouseX, y:mouseY, color: flowerColor, nPetals: nPetals, size: size})
-    //opções de cor
     drawFlower(mouseX,mouseY,flowerColor)
     flowerCount += 1
   }  
@@ -153,6 +161,13 @@ function drawFlower(x,y, flowerColor,nPetals,size) {
   translate(x,y)
   noStroke()
 
+  push()
+  drawingContext.shadowOffsetX = 20;
+  drawingContext.shadowOffsetY = 15;
+  drawingContext.shadowBlur = 30
+  drawingContext.shadowColor = 'black';
+  circle(0,0,20)
+  pop()
   for(i=0;i<nPetals;i++){
     fill(flowerColor)
     ellipse(0,15, size/3, size)
@@ -170,8 +185,8 @@ function addFish(){
   rotate (-angle);
   
   fill("orange")
-  ellipse(radius1,0, 10,16)
-  triangle(radius1,8,radius1-8,15,radius1+8,15)
+  ellipse(radius[0],0, 10,16)
+  triangle(radius[0],8,radius[0]-8,15,radius[0]+8,15)
   
   angle = angle + speed;
   pop()
@@ -181,11 +196,26 @@ function addFish(){
   rotate (angle);
   
   fill("orange")
-  ellipse(radius2,0, 10,16)
-  triangle(radius2,-8,radius2-8,-15,radius2+8,-15)
+  ellipse(radius[1],0, 10,16)
+  triangle(radius[1],-8,radius[1]-8,-15,radius[1]+8,-15)
   
   angle = angle + speed*1.5;
   pop()
 
-}
+  push()
+  translate(centerX, centerY);
+  rotate (angle);
+  
+  strokeWeight(2)
+  stroke(255,255,255,150)
+  noFill()
+  // circle(radius[0]/10,radius[0]/4, 40)
+  arc(radiusWaves[0], radiusWaves[0]/4, 50, 50, 0, HALF_PI);
+  arc(radiusWaves[1], radiusWaves[1]/3, 150, 150, 0, HALF_PI);
+  arc(radiusWaves[0]/8, radiusWaves[0]/3, 180, 180, PI, PI + QUARTER_PI);
+  arc(radiusWaves[0]/8, radiusWaves[0]/3, 100, 100, HALF_PI, PI);
+  
+  angle = angle + speed;
+  pop()
 
+}
